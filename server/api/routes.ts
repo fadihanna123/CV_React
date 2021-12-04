8a29ef146c1566095280ba2335f7780d36d968da
fadihanna123 <fhanna181@gmail.com>
2021-12-04 02:10:27 +0100

    Make code more cleaner

--- server/api/routes.ts
+++ server/api/routes.ts
@@ -1,31 +1,9 @@
-import cors from "cors";
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 
-import { transporter } from "../server";
-import { IMailData } from "../typings";
+import { sendMail } from "../controllers";
 
 const router: Router = Router();
 
-router.post("/mailit", (req: Request, res: Response) => {
-  const { mail, fullname, phone, msg } = req.body;
-
-  const mailData: IMailData = {
-    from: `${fullname} XX@gmail.com`,
-    to: "XX@gmail.com",
-    subject: `${fullname} vill komma i kontakt med dig`,
-    html: `Fullständigt namn: ${fullname}<br />
-    E-postadress: ${mail}<br />
-    Mobilnummer: ${phone}<br />
-    Meddelande: ${msg}<br />`,
-  };
-
-  transporter.sendMail(mailData, (error, info) => {
-    if (error) {
-      return console.log((error as Error).message);
-    }
-
-    res.status(200).send({ message: "Mail send", message_id: info.messageId });
-  });
-});
+router.post("/mailit", sendMail);
 
 export default router;
---
+++ server/controllers/sendMail.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+
+import { transporter } from "../server";
+import { IMailData } from "../typings";
+
+export const sendMail = (req: Request, res: Response): void => {
+  const { mail, fullname, phone, msg } = req.body;
+
+  const mailData: IMailData = {
+    from: `${fullname} XX@gmail.com`,
+    to: "XX@gmail.com",
+    subject: `${fullname} vill komma i kontakt med dig`,
+    html: `Fullständigt namn: ${fullname}<br />
+    E-postadress: ${mail}<br />
+    Mobilnummer: ${phone}<br />
+    Meddelande: ${msg}<br />`,
+  };
+
+  transporter.sendMail(mailData, (error, info) => {
+    if (error) {
+      return console.log((error as Error).message);
+    }
+
+    res.status(200).send({ message: "Mail send", message_id: info.messageId });
+  });
+};
