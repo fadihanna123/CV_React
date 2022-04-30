360be06066ea6e3dde083d32c68a3ea6c5180912
FadiHanna <fadi.hanna@rektron.se>
2022-05-01 00:10:43 +0200

    Deploy Test

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -37,7 +37,7 @@ const About: React.FC = () => (
                 src={ProfileBild}
                 aria-label="Profilbild"
                 loading="lazy"
-                alt="TEST"
+                alt="Profilbild"
             />
         </Col25>
     </TxtRow>
