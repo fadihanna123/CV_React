331f659b02a0d329f891cb96b8adad891bca19da
FadiHanna <fadi.hanna@rektron.se>
2022-09-26 03:43:29 +0200

    Upgrade packages

--- client/src/redux/store/index.ts
+++ client/src/redux/store/index.ts
@@ -1,5 +1,8 @@
-import { legacy_createStore as createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { allReducers } from 'redux/reducers';
 
-export const store = createStore(allReducers, composeWithDevTools());
+export const store = createStore(
+  allReducers,
+  composeWithDevTools(applyMiddleware())
+);
